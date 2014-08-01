9cae70fd76811d1ef32355cbb75483ffa1b9fb6d
Wong Hoi Sing Edison <hswong3i@gmail.com>
2014-08-01 11:03:20 +0800

    Cleanup legacy config.

--- install.sh
+++ install.sh
@@ -2,29 +2,23 @@
 
 set -o xtrace
 
-# Install XCode.
-xcode-select --install
-sudo xcodebuild -license
+# Define variables.
+BRANCH="master"
+TMP_DIR=`mktemp -d -t homebrew-installer.XXXXXX`
 
 # Install Homebrew.
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 brew update && brew upgrade && brew doctor
-brew install wget git curl coreutils
-export PATH=/usr/local/opt/coreutils/libexec/gnubin:$PATH
 
-# Define variables.
-BRANCH="master"
-TMP_DIR=`mktemp -d`
-REPO_DIR="$TMP_DIR/homebrew-installer"
+# Install dep packages.
+brew install wget git curl coreutils
+PATH=/usr/local/opt/coreutils/libexec/gnubin:$PATH
 
 # Install Homebrew Cask.
 brew install --HEAD caskroom/cask/brew-cask
 brew update && brew upgrade && brew doctor
 
 # Install Homebrew-PHP.
-brew untap homebrew/dupes 
-brew untap homebrew/versions
-brew untap homebrew/homebrew-php
 brew tap homebrew/dupes
 brew tap homebrew/versions
 brew tap homebrew/homebrew-php
@@ -32,15 +26,15 @@ brew update && brew upgrade && brew doctor
 brew install php55
 
 # Clone repo into temp folder.
-git clone https://github.com/pantarei/homebrew-installer.git $REPO_DIR
+git clone https://github.com/pantarei/homebrew-installer.git $TMP_DIR
 
 # Copy .bash_profile.
-cp $REPO_DIR/.bash_profile $HOME/
+cp $TMP_DIR/.bash_profile $HOME/
 source $HOME/.bash_profile
 
 # Install packages.
-cat $REPO_DIR/homebrew.list | xargs brew install --force -
-cat $REPO_DIR/homebrew-cask.list | xargs brew cask install --force -
+cat $TMP_DIR/homebrew.list | xargs brew install --force -
+cat $TMP_DIR/homebrew-cask.list | xargs brew cask install --force -
 
 # Post-install cleanup
 sudo /bin/cp -RfX /usr/local/opt/osxfuse/Library/Filesystems/osxfusefs.fs /Library/Filesystems
