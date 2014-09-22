769c7f61b8cc4ac7900b31b60afb8b9ee7d6ce0f
Wong Hoi Sing Edison <hswong3i@gmail.com>
2014-09-22 11:58:19 +0800

    Whoops, the Homebrew installer has moved!

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ BRANCH="master"
 TMP_DIR=`mktemp -d -t homebrew-installer.XXXXXX`
 
 # Install Homebrew.
-ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update && brew upgrade && brew doctor
 
 # Install dep packages.
