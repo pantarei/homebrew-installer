58584939cab5f6ade1cb1af3fbef9d09feb83733
Wong Hoi Sing Edison <hswong3i@gmail.com>
2015-10-15 12:19:16 +0800

    Also install uTorrent before end of installation.

--- install.sh
+++ install.sh
@@ -14,26 +14,19 @@ brew update && brew upgrade --all && brew cleanup && brew prune && brew doctor
 brew install caskroom/cask/brew-cask
 brew update && brew upgrade --all && brew cleanup && brew prune && brew doctor
 
+# Tap all required projects.
+brew tap caskroom/fonts
+brew tap caskroom/versions
+brew tap homebrew/completions
+brew tap homebrew/dupes
+brew tap homebrew/homebrew-php
+brew tap homebrew/versions
+brew update && brew upgrade --all && brew cleanup && brew prune && brew doctor
+
 # Install dep packages.
-brew install wget git curl coreutils
+brew install wget git curl coreutils php56
 brew cask install xquartz
 PATH=/usr/local/opt/coreutils/libexec/gnubin:$PATH
-
-# Install Homebrew-PHP.
-brew tap homebrew/dupes
-brew tap homebrew/versions
-brew tap homebrew/homebrew-php
-brew update && brew upgrade --all && brew cleanup && brew prune && brew doctor
-brew install php56
-
-# Install Caskroom-fonts.
-brew tap caskroom/fonts
-
-# Install Homebrew-completions.
-brew tap homebrew/completions
-
-# Install homebrew-cask-versions.
-brew tap caskroom/versions
 
 # Clone repo into temp folder.
 git clone https://github.com/pantarei/homebrew-installer.git $TMP_DIR
@@ -53,6 +46,7 @@ bash <(curl -sL https://raw.githubusercontent.com/pantarei/composer-installer/ma
 bash <(curl -sL https://raw.githubusercontent.com/pantarei/npm-installer/master/install.sh)
 
 # Post-install cleanup
+open /opt/homebrew-cask/Caskroom/utorrent/latest/uTorrent.app
 ln -sfv /usr/local/opt/tor/*.plist ~/Library/LaunchAgents
 launchctl load ~/Library/LaunchAgents/homebrew.mxcl.tor.plist
 defaults write com.apple.dock ResetLaunchPad -bool true; killall Dock
